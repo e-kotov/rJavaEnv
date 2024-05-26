41a03980399d70e3fcb3f6ce69d9d363070d6525
Egor Kotov <kotov.egor@gmail.com>
2024-05-27 00:02:01 +0200

    add verbose messages to download_java and add listing and clearing the cache folder

--- R/download_java.R
+++ R/download_java.R
@@ -15,12 +15,20 @@
 #' download_java(distribution = "Corretto")
 #' download_java()
 #' }
-download_java <- function(version = "17",
+download_java <- function(version = 21,
                           distribution = "Corretto",
                           dest_dir = tools::R_user_dir("rJavaEnv", which = "cache"),
                           platform = .detect_platform()$os,
-                          arch = .detect_platform()$arch) {
+                          arch = .detect_platform()$arch,
+                          verbose = TRUE) {
   java_urls <- .load_java_urls()
+
+  # print out the detected platform and architecture
+  if (verbose) {
+    message(sprintf("Detected platform: %s", platform))
+    message(sprintf("Detected architecture: %s", arch))
+    message("You can change the platform and architecture by specifying the 'platform' and 'arch' arguments.")
+  }
 
   if (!distribution %in% names(java_urls)) {
     stop("Unsupported distribution")
@@ -52,4 +60,53 @@ download_java <- function(version = "17",
   }
 
   return(dest_file)
+}
+
+#' List the contents of the Java distributions cache folder
+#'
+#' @param cache_dir The cache directory to list. Defaults to the user-specific data directory.
+#'
+#' @return A character vector with the contents of the cache directory.
+#' @export
+#'
+#' @examples list_java_distributions_cache()
+list_java_distributions_cache <- function(
+    cache_dir = tools::R_user_dir("rJavaEnv", which = "cache")) {
+  if (!dir.exists(cache_dir)) {
+    message("Path does not exist")
+  }
+  message("Contents of the Java distributions cache folder:")
+  list.files(cache_dir) # todo: output as a nicely formatted table
+}
+
+#' Clear the Java distributions cache folder
+#'
+#' @param cache_dir The cache directory to clear. Defaults to the user-specific data directory.
+#'
+#' @return A message indicating whether the cache was cleared or not.
+#' @export
+#'
+#' @examples clear_java_distributions_cache()
+clear_java_distributions_cache <- function(
+    cache_dir = tools::R_user_dir("rJavaEnv", which = "cache"),
+    check = TRUE,
+    confirm = TRUE) {
+  if (dir.exists(cache_dir)) {
+    if (confirm) {
+      if (check) {
+        print(list_java_distributions_cache(cache_dir))
+      }
+      message("Are you sure you want to clear the Java distributions cache? (yes/no)")
+      response <- readline()
+      if (tolower(response) != "yes") {
+        # exit with message
+        message("Java distributions cache was not cleared.")
+        return(invisible(NULL))
+      }
+    }
+    unlink(cache_dir, recursive = TRUE)
+    message("Java distributions cache cleared.")
+  } else {
+    message("Java distributions cache is already empty.")
+  }
 }
