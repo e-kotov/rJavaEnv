8fc93883ccdfb796194dc278318472d1ddb66296
Egor Kotov <kotov.egor@gmail.com>
2024-05-28 00:04:33 +0200

    set and unset java env functions

--- R/java_env.R
+++ R/java_env.R
@@ -1,17 +1,98 @@
+# set java env ------------------------------------------------------------
+
+
 #' Set the JAVA_HOME and PATH environment variables to a given path
 #'
-#' @param java_dir
-#'
+#' @param java_home The path to the desired JAVA_HOME.
+#' @param where Where to set the JAVA_HOME: "session", "project", or "both". Defaults to "both". When "both" or "project" is selected, the function updates the .Rprofile file in the project directory to set the JAVA_HOME and PATH environment variables at the start of the R session.
 #' @export
 #' @return Nothing. Sets the JAVA_HOME and PATH environment variables.
-set_java_env <- function(java_home) {
+set_java_env <- function(java_home, where = c("both", "session", "project")) {
+  where <- match.arg(where)
+
+  if (where %in% c("session", "both")) {
+    # Set environment variables for the current session
+    .set_java_paths(java_home)
+    message(sprintf("JAVA_HOME set to %s", java_home))
+  }
+
+  if (where %in% c("project", "both")) {
+    # Update the .Rprofile in the project directory
+    .set_java_home_in_rprofile(java_home)
+  }
+
+  invisible(NULL)
+}
+
+#' Set the JAVA_HOME and PATH environment variables
+#'
+#' @param java_home The path to the desired JAVA_HOME.
+#' @keywords internal
+.set_java_paths <- function(java_home) {
   Sys.setenv(JAVA_HOME = java_home)
   old_path <- Sys.getenv("PATH")
   new_path <- file.path(java_home, "bin")
   Sys.setenv(PATH = paste(new_path, old_path, sep = .Platform$path.sep))
-  message(sprintf("JAVA_HOME set to %s", java_home))
-  return(invisible(NULL))
 }
+
+#' Update the .Rprofile file in the project directory
+#'
+#' @param java_home The path to the desired JAVA_HOME.
+#' @keywords internal
+.set_java_home_in_rprofile <- function(java_home) {
+  unset_java_env(quiet = TRUE)
+
+  project <- getwd()
+  rprofile_path <- file.path(project, ".Rprofile")
+  lines_to_add <- c(
+    "# rJavaEnv begin: Manage JAVA_HOME",
+    sprintf("Sys.setenv(JAVA_HOME = '%s') # rJavaEnv", java_home),
+    "old_path <- Sys.getenv('PATH') # rJavaEnv",
+    "new_path <- file.path(Sys.getenv('JAVA_HOME'), 'bin') # rJavaEnv",
+    "Sys.setenv(PATH = paste(new_path, old_path, sep = .Platform$path.sep)) # rJavaEnv",
+    "# rJavaEnv end: Manage JAVA_HOME"
+  )
+
+  if (file.exists(rprofile_path)) {
+    cat(lines_to_add, file = rprofile_path, append = TRUE, sep = "\n")
+    message(sprintf("Set JAVA_HOME to '%s' in .Rprofile in '%s'", java_home, rprofile_path))
+  } else {
+    writeLines(lines_to_add, con = rprofile_path)
+    message(sprintf(".Rprofile created with JAVA_HOME settings in '%s'", rprofile_path))
+  }
+}
+
+
+
+# unset java env ----------------------------------------------------------
+
+#' Unset the JAVA_HOME and PATH environment variables in the project .Rprofile
+#'
+#' @export
+#' @return Nothing. Removes the JAVA_HOME and PATH environment variables settings from the project .Rprofile.
+unset_java_env <- function(quiet = FALSE) {
+  project <- getwd()
+  rprofile_path <- file.path(project, ".Rprofile")
+
+  if (file.exists(rprofile_path)) {
+    rprofile_content <- readLines(rprofile_path, warn = FALSE)
+    rprofile_content <- rprofile_content[!grepl("# rJavaEnv", rprofile_content)]
+    writeLines(rprofile_content, con = rprofile_path)
+    if (!quiet) {
+      message(sprintf("Removed JAVA_HOME settings from .Rprofile in '%s'", rprofile_path))
+    }
+  } else {
+    if (!quiet) {
+      message(sprintf("No .Rprofile found in '%s'", project))
+    }
+  }
+}
+
+
+
+
+# check java version ------------------------------------------------------
+
 
 #' Check Java Version with a Specified JAVA_HOME Using a Separate R Session
 #'
@@ -21,17 +102,16 @@ set_java_env <- function(java_home) {
 #' having to reload the current R session. The reason for this is that once Java is initialized in an R session, it
 #' cannot be uninitialized unless the current R session is restarted.
 #'
-#' @param java_home The path to the desired JAVA_HOME.
+#' @param java_home The path to the desired JAVA_HOME. If NULL, uses the current JAVA_HOME environment variable.
 #' @return A character string with the Java version.
 #' @examples
 #' \dontrun{
 #' check_java_version_rjava("/new/path/to/java")
 #' }
 #' @export
-check_java_version_rjava <- function(java_home) {
-  if (!dir.exists(java_home)) {
-    stop("Invalid JAVA_HOME path: Directory does not exist.")
-  }
+check_java_version_rjava <- function(java_home = NULL) {
+  # Use the helper function to set JAVA_HOME and check java executable
+  .set_and_check_java_home(java_home)
 
   # Get the code of the unexported function to use a script
   internal_function <- getFromNamespace(".check_java_version_rscript", "rJavaEnv")
@@ -47,11 +127,71 @@ check_java_version_rjava <- function(java_home) {
   writeLines(wrapper_script, script_file)
 
   output <- system2("Rscript",
-    args = c(script_file, java_home),
+    args = c(script_file, Sys.getenv("JAVA_HOME")),
     stdout = TRUE, stderr = TRUE
   )
 
   unlink(script_file)
 
   return(cat(output))
+}
+
+
+#' Check installed Java version using terminal commands
+#'
+#' @param java_home Path to Java home directory. If NULL, the function uses the JAVA_HOME environment variable.
+#'
+#' @return Java version, otherwise stops with an error.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' check_java_version_cmd()
+#' }
+check_java_version_cmd <- function(java_home = NULL) {
+  # Use the helper function to set JAVA_HOME and check java executable
+  .set_and_check_java_home(java_home)
+
+  # Check Java path and version using system commands
+  .check_java_system2()
+}
+
+#' Check and print Java path and version
+#'
+#' This function checks the Java executable path and retrieves the Java version,
+#' then prints these details to the console.
+#'
+#' @return Nothing. Messages the Java path and version to the console.
+.check_java_system2 <- function() {
+  which_java <- system2("which", args = "java", stdout = TRUE, stderr = TRUE)
+  java_ver <- system2("java", args = "-version", stdout = TRUE, stderr = TRUE)
+  message(sprintf("Java path: %s", which_java))
+  message(sprintf("Java version:\n%s", paste(java_ver, collapse = "\n")))
+}
+
+
+#' Helper function to set JAVA_HOME and check if java executable exists
+#'
+#' @param java_home The path to the desired JAVA_HOME. If NULL, uses the current JAVA_HOME environment variable.
+#' @keywords internal
+.set_and_check_java_home <- function(java_home = NULL) {
+  # Set JAVA_HOME and PATH if java_home is provided
+  if (!is.null(java_home)) {
+    set_java_env(java_home)
+  }
+
+  # Get JAVA_HOME and check if it's set
+  current_java_home <- Sys.getenv("JAVA_HOME")
+  if (current_java_home == "") {
+    warning("JAVA_HOME is not set.")
+  } else {
+    message(sprintf("JAVA_HOME: %s", current_java_home))
+  }
+
+  # Check if java executable exists in the PATH
+  if (!nzchar(Sys.which("java"))) {
+    stop("Java installation is not valid.")
+  }
+
+  return(TRUE)
 }
