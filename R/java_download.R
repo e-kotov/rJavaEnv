e3ff35860a745fce79cdba1fe6a8de0b5b6ee086
Egor Kotov <kotov.egor@gmail.com>
2024-05-29 13:34:03 +0200

    replace messages with cli functions

--- R/java_download.R
+++ R/java_download.R
@@ -23,22 +23,20 @@ java_download <- function(version = 21,
                           arch = rJavaEnv:::.detect_platform()$arch,
                           verbose = TRUE) {
 
-  # Load java urls data -----------------------------------------------------
-
   java_urls <- rJavaEnv:::.load_java_urls()
 
   valid_distributions <- names(java_urls)
   valid_platforms <- names(java_urls[[distribution]])
   valid_architectures <- names(java_urls[[distribution]][[platform]])
 
-  # Checks function parameters ---------------------------------------------
-
+  # Checks for the parameters
   checkmate::assert(
     checkmate::check_integerish(version, lower = 1),
     checkmate::check_character(version, pattern = "^[0-9]+$")
   )
   checkmate::assert_choice(distribution, valid_distributions)
 
+  # Check if the directory exists, if not, create it
   if (!dir.exists(dest_dir)) {
     dir.create(dest_dir, recursive = TRUE)
   }
@@ -48,24 +46,25 @@ java_download <- function(version = 21,
   checkmate::assert_choice(arch, valid_architectures)
   checkmate::assert_flag(verbose)
 
-
-  # print out the detected platform and architecture
+  # Print out the detected platform and architecture
   if (verbose) {
-    message(sprintf("Detected platform: %s", platform))
-    message(sprintf("Detected architecture: %s", arch))
-    message("You can change the platform and architecture by specifying the 'platform' and 'arch' arguments.")
+    cli::cli_inform(c(
+      "Detected platform: {.strong {platform}}",
+      "Detected architecture: {.strong {arch}}",
+      "You can change the platform and architecture by specifying the {.arg platform} and {.arg arch} arguments."
+    ))
   }
 
   if (!distribution %in% names(java_urls)) {
-    stop("Unsupported distribution")
+    cli::cli_abort("Unsupported distribution: {.val {distribution}}")
   }
 
   if (!platform %in% names(java_urls[[distribution]])) {
-    stop("Unsupported platform")
+    cli::cli_abort("Unsupported platform: {.val {platform}}")
   }
 
   if (!arch %in% names(java_urls[[distribution]][[platform]])) {
-    stop("Unsupported architecture")
+    cli::cli_abort("Unsupported architecture: {.val {arch}}")
   }
 
   url_template <- java_urls[[distribution]][[platform]][[arch]]
@@ -73,16 +72,13 @@ java_download <- function(version = 21,
 
   dest_file <- file.path(dest_dir, basename(url))
 
-  message(sprintf("Downloading Java %s (%s) for %s %s to %s", version, distribution, platform, arch, dest_file))
+  cli::cli_inform("Downloading Java {version} ({distribution}) for {platform} {arch} to {dest_file}")
 
   if (file.exists(dest_file)) {
-    message("File already exists. Skipping download.")
+    cli::cli_alert_info("File already exists. Skipping download.")
   } else {
-    if (!dir.exists(dest_dir)) {
-      dir.create(dest_dir, recursive = TRUE)
-    }
     curl::curl_download(url, dest_file, quiet = FALSE)
-    message("Download completed.")
+    cli::cli_alert_success("Download completed.")
   }
 
   return(dest_file)
