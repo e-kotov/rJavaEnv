ac1fdbb3ced0535ac83162faf16b0627cdda26d2
Egor Kotov <kotov.egor@gmail.com>
2024-08-19 17:42:28 +0200

    unlink md5 files along with distribs

--- R/java_manage_distrib_cache.R
+++ R/java_manage_distrib_cache.R
@@ -1,24 +1,23 @@
 #' List the contents of the Java distributions cache folder
 #'
 #' @param output The format of the output: "data.frame" or "vector". Defaults to "data.frame".
-#' @param verbose Whether to print detailed messages. Defaults to FALSE.
-#' @param cache_dir The cache directory to list. Defaults to the user-specific data directory.
+#' @inheritParams java_list
+#' @inheritParams java_download
 #' @return A character vector with the contents of the cache directory.
-#' @export
 #'
 #' @examples
 #' java_list_distrib_cache()
 #'
 java_list_distrib_cache <- function(
-    cache_dir = tools::R_user_dir("rJavaEnv", which = "cache")
-    output = c("data.frame", "vector"),
-    verbose = FALSE
+  cache_path = tools::R_user_dir("rJavaEnv", which = "cache"),
+  output = c("data.frame", "vector"),
+  verbose = FALSE
 ) {
   output <- match.arg(output)
 
-  cache_dir <- file.path(cache_dir, "distrib")
+  cache_path <- file.path(cache_path, "distrib")
 
-  if (!dir.exists(cache_dir)) {
+  if (!dir.exists(cache_path)) {
     cli::cli_alert_danger("Path does not exist")
     return(character(0))
   }
@@ -26,7 +25,7 @@ java_list_distrib_cache <- function(
 
   java_distrs <- grep(
     "md5$",
-    list.files(cache_dir, full.names = TRUE),
+    list.files(cache_path, full.names = TRUE),
     invert = TRUE,
     value = TRUE
   )
@@ -41,37 +40,44 @@ java_list_distrib_cache <- function(
 
 #' Clear the Java distributions cache folder
 #'
-#' @param cache_dir The cache directory to clear. Defaults to the user-specific data directory.
-#' @param check Whether to list the contents of the cache directory before clearing it. Defaults to TRUE.
-#' @param delete_all Whether to delete all distributions without prompting. Defaults to FALSE.
+#' @param cache_path The cache directory to clear. Defaults to the user-specific data directory.
+#' @inheritParams java_clear
+#' @inheritParams java_download
 #' @return A message indicating whether the cache was cleared or not.
-#' @export
 #'
 #' @examples
 #' \dontrun{
-#' java_clear_distrib_cache()
+#' # delete all cached distributions, leave cache path empty to clear in the package cache in your default cache user space
+#' java_clear_distrib_cache(
+#'   cache_path = tempdir(),
+#'   check = FALSE,
+#'   delete_all = TRUE
+#' )
 #' }
 java_clear_distrib_cache <- function(
-    check = TRUE,
-    delete_all = FALSE,
-    cache_dir = tools::R_user_dir("rJavaEnv", which = "cache")) {
+  cache_path = tools::R_user_dir("rJavaEnv", which = "cache"),
+  check = TRUE,
+  delete_all = FALSE
+) {
   rje_consent_check()
   
-  distrib_cache_dir <- file.path(cache_dir, "distrib")
+  distrib_cache_path <- file.path(cache_path, "distrib")
 
-  if (!dir.exists(distrib_cache_dir)) {
-    cli::cli_inform("Java distributions cache is already empty.")
-    return(invisible(NULL))
+  if (!dir.exists(distrib_cache_path)) {
+    if (length(list.files(distrib_cache_path)) == 0) {
+      cli::cli_inform("Java distributions cache is already empty.")
+      return(invisible(NULL))
+    }
   }
 
   if (delete_all) {
-    unlink(file.path(distrib_cache_dir, "*"), recursive = TRUE)
+    unlink(file.path(distrib_cache_path, "*"), recursive = TRUE)
     cli::cli_inform("Java distributions cache cleared.")
     return(invisible(NULL))
   }
 
   if (check) {
-    distributions <- java_list_distrib_cache(output = "vector", cache_dir = cache_dir)
+    distributions <- java_list_distrib_cache(output = "vector", cache_path = cache_path)
     if (length(distributions) == 0) {
       cli::cli_inform("No Java distributions found to clear.")
       return(invisible(NULL))
@@ -86,7 +92,7 @@ java_clear_distrib_cache <- function(
     response <- readline()
 
     if (tolower(response) == "all") {
-      unlink(file.path(distrib_cache_dir, "*"), recursive = TRUE)
+      unlink(file.path(distrib_cache_path, "*"), recursive = TRUE)
       cli::cli_inform("All Java distributions have been cleared.")
     } else {
       choice <- suppressWarnings(as.integer(response))
@@ -94,6 +100,8 @@ java_clear_distrib_cache <- function(
         cli::cli_inform("No distributions were cleared.")
       } else {
         unlink(distributions[choice], recursive = TRUE)
+        md5_file <- paste0(distributions[choice], "md5")
+        if(file.exists(md5_file)) unlink(md5_file)
         cli::cli_inform("Java distribution {choice} has been cleared.")
       }
     }
@@ -101,7 +109,7 @@ java_clear_distrib_cache <- function(
     cli::cli_alert_info("Are you sure you want to clear the Java distributions cache? (yes/no)")
     response <- readline()
     if (tolower(response) == "yes") {
-      unlink(file.path(distrib_cache_dir, "*"), recursive = TRUE)
+      unlink(file.path(distrib_cache_path, "*"), recursive = TRUE)
       cli::cli_inform("Java distributions cache cleared.")
     } else {
       cli::cli_inform("Java distributions cache was not cleared.")
