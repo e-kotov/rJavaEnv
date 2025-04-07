693f4a294aab0252a702d0529a9a8a6a593701ee
chainsawriot <chainsawtiney@gmail.com>
2025-04-07 15:32:47 +0200

    Correct the tests introduced #77
    h/t @mrwunderbar666 for detecting them

--- tests/testthat/test-java_unpack.R
+++ tests/testthat/test-java_unpack.R
@@ -3,15 +3,15 @@ test_that("bad java_distrib_path", {
   expect_error(java_unpack(bad_path))
 
   bad_path <- "/home/johndoe/.cache/R/rJavaEnv/distrib/amazon-corretto-21-x64-linux-jdk.tar.7z"
-  expect_error(java_unpack(fake_path, "Unsupported file format"))
+  expect_error(java_unpack(bad_path), "Unsupported file format")
 
   bad_path <- "/home/johndoe/.cache/R/rJavaEnv/distrib/amazon-corretto-x64-linux-jdk.tar.zip"
-  expect_error(java_unpack(fake_path, "Java version"))
+  expect_error(java_unpack(bad_path), "Java version")
 
-  bad_path <- "/home/johndoe/.cache/R/rJavaEnv/distrib/amazon-corretto-linux-jdk.tar.zip"
-  expect_error(java_unpack(fake_path, "architecture"))
+  bad_path <- "/home/johndoe/.cache/R/rJavaEnv/distrib/amazon-corretto-21-linux-jdk.tar.zip"
+  expect_error(java_unpack(bad_path), "architecture")
 
   bad_path <- "/home/johndoe/.cache/R/rJavaEnv/distrib/amazon-corretto-21-x64-msdos-jdk.tar.zip"
-  expect_error(java_unpack(fake_path, "platform"))
+  expect_error(java_unpack(bad_path), "platform")
 
 })
